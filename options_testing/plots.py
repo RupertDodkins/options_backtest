e7204b9c9804fc611fa6a6196942dcc138423a97
rdodkins <rupertdodkins@gmail.com>
2022-09-05 15:14:49 -0700

    plot linechart added

--- options_testing/plots.py
+++ options_testing/plots.py
@@ -1,12 +1,7 @@
 import plotly.graph_objects as go
+import numpy as np
 
-def plot_candles(df, show_afterhours=False):
-    fig = go.Figure(data=[go.Candlestick(x=df.index,
-            open=df['open'],
-            high=df['high'],
-            low=df['low'],
-            close=df['close'])])
-
+def update_fig(fig, show_afterhours=False):
     if not show_afterhours:
         fig.update_xaxes(
             rangeslider_visible=True,
@@ -24,3 +19,28 @@ def plot_candles(df, show_afterhours=False):
 
     fig.update_layout(xaxis_rangeslider_visible=False)
     fig.show()
+
+def plot_timeseries(df, yval=None, show_afterhours=False):
+    ohlc = ['open', 'high', 'low', 'close']
+    if np.in1d(df.columns, ohlc).sum() == 4:
+        if not yval:
+            yval = ohlc
+        plot_candles(df, yval, show_afterhours=show_afterhours)
+    else:
+        if not yval:
+            yval = 'close'
+        plot_line(df, yval, show_afterhours=show_afterhours)
+
+def plot_line(df, value='option value', show_afterhours=False):
+    fig = go.Figure(data=[go.Scatter(x=df.index, y=df[value])])
+    update_fig(fig, show_afterhours=show_afterhours)
+
+def plot_candles(df, value=None, show_afterhours=False):
+    if not value:
+        value = ['open', 'high', 'low', 'close']
+    fig = go.Figure(data=[go.Candlestick(x=df.index,
+            open=df[value[0]],
+            high=df[value[1]],
+            low=df[value[2]],
+            close=df[value[3]])])
+    update_fig(fig, show_afterhours=show_afterhours)

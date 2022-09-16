6eedb5d22ea1f0fdea7f2f9aa25027139b197467
rdodkins <rupertdodkins@gmail.com>
2022-09-16 09:33:38 -0700

    full tsla hourly code now available

--- options_testing/quantconnect.py
+++ options_testing/quantconnect.py
@@ -13,3 +13,22 @@ def load_tsla_minutely(data_dir=None, suffix=None):
         tsla[col] = np.loadtxt(tsla_col_loc, delimiter=',', dtype=dtype)
     tsla = tsla.set_index('date')
     return tsla
+
+def load_tsla_hourly(data_dir=None, suffix=None):
+    df_loc = '/Users/dodkins/PythonProjects/stocks/options_testing/data/tsla_hourly_2011+'
+    if os.path.exists(df_loc):
+        tsla = pd.read_csv(df_loc)
+    else:
+        if not data_dir:
+            data_dir = '/Users/dodkins/PythonProjects/stocks/options_testing/data/'
+        if not suffix:
+            suffix = 'tsla_hourly_'
+        cols = ['datetime', 'open', 'high', 'low', 'close']
+        tsla = pd.DataFrame(columns=cols)
+        for col, dtype in zip(cols, [str, float, float, float, float]):
+            tsla_col_loc = os.path.join(data_dir, f'{suffix}{col}.txt')
+            data = np.loadtxt(tsla_col_loc, delimiter=',', dtype=dtype)
+            print(tsla_col_loc, col, dtype, len(data))
+            tsla[col] = data
+        tsla = tsla.set_index('datetime')
+    return tsla

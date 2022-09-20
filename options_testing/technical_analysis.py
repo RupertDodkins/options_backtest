f1fea8771c6e4bd1c2b5641eae9f29e335b61000
rdodkins <rupertdodkins@gmail.com>
2022-09-19 22:33:49 -0700

    remove non working volume profile code

--- options_testing/technical_analysis.py
+++ options_testing/technical_analysis.py
@@ -1,29 +1,6 @@
 import pandas as pd
 import numpy as np
 
-def vpNoDF(df):
-    Step = 0.00001
-    Precision = 5
-    _low = 85.0
-    _high = 116.4
-    # Set the complete index of prices +/- 1 step due to weird floating point precision issues
-    volume_prices = pd.Series(0, index=np.around(np.arange(_low - Step, _high + Step, Step), decimals=Precision))
-    time_prices = volume_prices.copy()
-    for index, state in df.iterrows():
-        _prices = np.around((state.high - state.low) / Step , 0)
-
-        # Evenly distribute the bar's volume over its range
-        volume_prices.loc[state.low:state.high] += state.volume / _prices
-        # Increment time at price
-        time_prices.loc[state.low:state.high] += 1
-
-    # Pandas only returns the 1st row of the max value,
-    # so we need to reverse the series to find the other side
-    # and then find the average price between those two extremes
-    volume_poc = (volume_prices.idxmax() + volume_prices.iloc[::-1].idxmax() / 2)
-    time_poc = (time_prices.idxmax() + time_prices.iloc[::-1].idxmax() / 2)
-    return volume_poc, time_poc
-
 def is_far_from_level(value, levels, df):
     ave = np.mean(df['high'] - df['low'])
     return np.sum([abs(value-level) < ave for _,level in levels]) == 0

21c966f8c515b930d84ea155100cb25753e9ca34
rdodkins <rupertdodkins@gmail.com>
2022-11-13 14:53:04 -0800

    Reordered functions

--- options_testing/quantconnect.py
+++ options_testing/quantconnect.py
@@ -3,39 +3,7 @@ import numpy as np
 import os
 from datetime import datetime, timedelta
 from plots import scatter_heatmap
-
-def load_tsla_minutely(data_dir=None, suffix=None):
-    if not data_dir:
-        data_dir = '/Users/dodkins/PythonProjects/stocks/options_testing/data/'
-    if not suffix:
-        suffix = 'tsla_minutely_'
-    tsla = pd.DataFrame(columns=['date', 'open','close'])
-    for col, dtype in zip(['date', 'open','close'], [str, float, float]):
-        tsla_col_loc = os.path.join(data_dir, f'{suffix}{col}.txt')
-        tsla[col] = np.loadtxt(tsla_col_loc, delimiter=',', dtype=dtype)
-    tsla = tsla.set_index('date')
-    return tsla
-
-def load_tsla_hourly(data_dir=None, suffix=None):
-    df_loc = '/Users/dodkins/PythonProjects/stocks/options_testing/data/tsla_hourly_2010+'
-    if os.path.exists(df_loc):
-        tsla = pd.read_csv(df_loc, index_col='datetime')
-    else:
-        if not data_dir:
-            data_dir = '/Users/dodkins/PythonProjects/stocks/options_testing/data/'
-        if not suffix:
-            suffix = 'tsla_hourly_'
-        cols = ['datetime', 'open', 'high', 'low', 'close', 'volume']
-        tsla = pd.DataFrame(columns=cols)
-        for col, dtype in zip(cols, [str, float, float, float, float, float]):
-            tsla_col_loc = os.path.join(data_dir, f'{suffix}{col}.txt')
-            data = np.loadtxt(tsla_col_loc, delimiter=',', dtype=dtype)[:21450]
-            print(col, len(data))
-            tsla[col] = data
-        tsla = tsla.set_index('datetime')
-        tsla = tsla.iloc[:-7]
-        tsla.to_csv(df_loc)
-    return tsla
+from utils import format_dates
 
 class QuantBookWrapper():
     def __init__(self, qc_vars=None):
@@ -151,6 +119,48 @@ class QuantBookWrapper():
         strikes.sort()
         opens = [qbw.option_history(s, datetime(*start), start=datetime(*expiration)).iloc[0]['open'] for s in strikes]
         plt.plot(strikes, opens)
+
+    def get_contract_symbols(self, start):
+        contract_symbols = self.qb.OptionChainProvider.GetOptionContractList(self.equity_symbol, start)
+        i = 1
+        while len(contract_symbols) == 0:
+            start -= timedelta(days=i)
+            contract_symbols = self.qb.OptionChainProvider.GetOptionContractList(self.equity_symbol, start)
+            i += 1
+        return contract_symbols
+
+def load_tsla_minutely(data_dir=None, suffix=None):
+    if not data_dir:
+        data_dir = '/Users/dodkins/PythonProjects/stocks/options_testing/data/'
+    if not suffix:
+        suffix = 'tsla_minutely_'
+    tsla = pd.DataFrame(columns=['date', 'open','close'])
+    for col, dtype in zip(['date', 'open','close'], [str, float, float]):
+        tsla_col_loc = os.path.join(data_dir, f'{suffix}{col}.txt')
+        tsla[col] = np.loadtxt(tsla_col_loc, delimiter=',', dtype=dtype)
+    tsla = tsla.set_index('date')
+    return tsla
+
+def load_tsla_hourly(data_dir=None, suffix=None):
+    df_loc = '/Users/dodkins/PythonProjects/stocks/options_testing/data/tsla_hourly_2010+'
+    if os.path.exists(df_loc):
+        tsla = pd.read_csv(df_loc, index_col='datetime')
+    else:
+        if not data_dir:
+            data_dir = '/Users/dodkins/PythonProjects/stocks/options_testing/data/'
+        if not suffix:
+            suffix = 'tsla_hourly_'
+        cols = ['datetime', 'open', 'high', 'low', 'close', 'volume']
+        tsla = pd.DataFrame(columns=cols)
+        for col, dtype in zip(cols, [str, float, float, float, float, float]):
+            tsla_col_loc = os.path.join(data_dir, f'{suffix}{col}.txt')
+            data = np.loadtxt(tsla_col_loc, delimiter=',', dtype=dtype)[:21450]
+            print(col, len(data))
+            tsla[col] = data
+        tsla = tsla.set_index('datetime')
+        tsla = tsla.iloc[:-7]
+        tsla.to_csv(df_loc)
+    return tsla
 
 if __name__ == "__main__":
     qbw = QuantBookWrapper({'qb': QuantBook(),'Resolution': Resolution,'OptionRight': OptionRight})
